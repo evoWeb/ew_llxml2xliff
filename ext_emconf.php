89b9f4a9ffa6f3a0ab211eebf6570b7e593de468
Sebastian Fischer <typo3@evoweb.de>
2024-05-06 21:30:30 +0200

    Allow TYPO3 13 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF['ew_llxml2xliff'] = [
     'version' => '5.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
